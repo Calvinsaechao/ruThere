53fe3821ff9afaa34584ca038549abafcbad90f3
Daniil Kistanov <daniilkistanov@csus.edu>
2018-07-01 18:34:43 -0700

    тут написанно по-русски

--- src/ruTherePackage/ruThere.java
+++ src/ruTherePackage/ruThere.java
@@ -38,31 +38,12 @@ import org.json.simple.parser.*;
 
 public class ruThere {
 
-	private static final String databaseLocation = 	//"/Users/kistanod/Projects/Java/ruThere/src/resources/database.json";
-													"D:/home/site/wwwroot/webapps/ROOT/WEB-INF/classes/resources/database.json";
-													//"C:/Users/Dennis/git/ruThere/src/resources/database.json";
-													//"C:/Users/Danny/git/ruThere/src/resources/database.json";
-													//"/Users/Calvin/IdeaProjects/ruThere/src/resources/database.json";
-													//"X:\\Desktop\\Eclipse-Workspace\\ruThere\\src\\resources\\database.json";
+	private static final String databaseLocation = "D:/home/site/wwwroot/webapps/ROOT/WEB-INF/classes/resources/database.json";
 	
+	private static final String clientSecretLocation = "D:/home/site/wwwroot/webapps/ROOT/WEB-INF/classes/resources/client_secret.json";										   
 	
-	
-	private static final String clientSecretLocation = //"/Users/kistanod/Projects/Java/ruThere/src/resources/client_secret.json";
-													   "D:/home/site/wwwroot/webapps/ROOT/WEB-INF/classes/resources/client_secret.json";
-													   //"C:/Users/Dennis/git/ruThere/src/resources/client_secret.json";
-													   //"C:/Users/Danny/git/ruThere/src/resources/client_secret.json";
-													   //"/Users/Calvin/IdeaProjects/ruThere/src/resources/client_secret.json";
-													   //"X:\\Desktop\\Eclipse-Workspace\\ruThere\\src\\resources\\client_secret.json";
-	
-	
-	
-	
-	private static final String credentialsLocation = //"/Users/kistanod/Projects/Java/ruThere/src/resources/credentials/sheets.googleapis.com-java-quickstart.json";
-													  "D:/home/site/wwwroot/webapps/ROOT/WEB-INF/classes/resources/credentials";
-													  //"C:/Users/Dennis/git/ruThere/src/resources/credentials/sheets.googleapis.com-java-quickstart.json";
-													  //"C:/Users/Danny/git/ruThere/src/resources/credentials/sheets.googleapis.com-java-quickstart.json";
-													  //"/Users/Calvin/IdeaProjects/ruThere/src/resources/credentials/sheets.googleapis.com-java-quickstart.json";
-													  //"X:\\Desktop\\Eclipse-Workspace\\ruThere\\src\\resources\\credentials\\sheets.googleapis.com-java-quickstart.json";
+	private static final String credentialsLocation = "D:/home/site/wwwroot/webapps/ROOT/WEB-INF/classes/resources/credentials";
+													 
 
     /** Application name. */
     private static final String APPLICATION_NAME = "ruThere";
